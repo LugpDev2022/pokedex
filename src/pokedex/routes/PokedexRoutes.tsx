4d1516e430ef56c7cfb65334b080816785632d90
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-29 17:59:04 -0600

    Set the correct pokemon/id route

--- src/pokedex/routes/PokedexRoutes.tsx
+++ src/pokedex/routes/PokedexRoutes.tsx
@@ -5,7 +5,7 @@ export const PokedexRoutes = () => (
   <Routes>
     <Route path="/" element={<PokedexPage />} />
     <Route path="/search" element={<SearchPage />} />
-    <Route path="/pokemon:id" element={<PokemonPage />} />
+    <Route path="/pokemon/:id" element={<PokemonPage />} />
 
     <Route path="/*" element={<Navigate to="/" />} />
   </Routes>
