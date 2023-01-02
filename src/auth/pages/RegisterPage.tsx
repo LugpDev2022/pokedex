b7bfa0b0b40b0bcffa0c1bbd10abc5ec8ce6d0f2
LugpDev2022 <lugpdev2022@gmail.com>
2023-01-02 09:00:12 -0600

    Implement useFormik in RegisterPage

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,22 +1,50 @@
+import { useFormik } from "formik";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AuthModal } from "../components/AuthModal";
 
 export const RegisterPage = () => {
+  //TODO: Add validators
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <AuthModal>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control
+            type="email"
+            id="email"
+            value={values.email}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control
+            type="text"
+            id="username"
+            value={values.username}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-4">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" />
+          <Form.Control
+            type="password"
+            id="password"
+            value={values.password}
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Button type="submit" className="w-100 px-0 mb-3">
