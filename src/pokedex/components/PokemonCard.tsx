fb291a384b4982fb22375f6be7bcd8281320ac99
LugpDev2022 <lugpdev2022@gmail.com>
2023-01-05 09:57:35 -0600

    Make the card a link

--- src/pokedex/components/PokemonCard.tsx
+++ src/pokedex/components/PokemonCard.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Card, Col, ListGroup, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { getPokemonType } from "../helpers";
 
 interface Props {
@@ -27,20 +28,25 @@ export const PokemonCard = ({
 
   return (
     <Col xs={11} sm={9} md={6} lg={3}>
-      <Card className="mt-5">
-        <Card.Header>
-          <Row>
-            <span className="text-uppercase mb-0 fw-bold fs-4">{name}</span>
-          </Row>
-          <span className="fs-5">ID: {id}</span>
-        </Card.Header>
-        <Card.Img src={sprite} />
-        <ListGroup variant="flush">
-          <ListGroup.Item className="fs-5">{pokemonTypes}</ListGroup.Item>
-          <ListGroup.Item className="fs-5">HEIGHT: {height}</ListGroup.Item>
-          <ListGroup.Item className="fs-5">WEIGHT: {weight}</ListGroup.Item>
-        </ListGroup>
-      </Card>
+      <Link
+        to={`/pokemon/${id}`}
+        className="text-decoration-none text-reset card-as-link"
+      >
+        <Card className="mt-5">
+          <Card.Header>
+            <Row>
+              <span className="text-uppercase mb-0 fw-bold fs-4">{name}</span>
+            </Row>
+            <span className="fs-5">ID: {id}</span>
+          </Card.Header>
+          <Card.Img src={sprite} />
+          <ListGroup variant="flush">
+            <ListGroup.Item className="fs-5">{pokemonTypes}</ListGroup.Item>
+            <ListGroup.Item className="fs-5">HEIGHT: {height}</ListGroup.Item>
+            <ListGroup.Item className="fs-5">WEIGHT: {weight}</ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Link>
     </Col>
   );
 };
