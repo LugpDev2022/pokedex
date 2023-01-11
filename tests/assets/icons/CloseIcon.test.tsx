91de76a8d1525d687e51f6107b390231bc8b341d
LugpDev2022 <lugpdev2022@gmail.com>
2023-01-11 11:49:22 -0600

    tests on <CloseIcon />

---
+++ tests/assets/icons/CloseIcon.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { CloseIcon } from "../../../src/assets/icons/CloseIcon";
+
+describe("tests on <CloseIcon />", () => {
+  test("should match snapshot", () => {
+    const { container } = render(<CloseIcon />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should have the given size", () => {
+    render(<CloseIcon size={75} />);
+
+    const svg = screen.getByTestId("svg");
+    expect(svg.getAttribute("width")).toBe("75");
+    expect(svg.getAttribute("height")).toBe("75");
+  });
+
+  test("should have the given size", () => {
+    render(<CloseIcon className="testing-class another-testing-class" />);
+
+    const svg = screen.getByTestId("svg");
+    expect(svg.classList.contains("testing-class")).toBeTruthy();
+    expect(svg.classList.contains("another-testing-class")).toBeTruthy();
+  });
+});
